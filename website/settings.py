3f350cbac8f428d8d4785da379d80da3cff88faf
charles <gitauwairimu@gmail.com>
2024-02-14 07:10:32 +0300

    locale and language translation

--- website/settings.py
+++ website/settings.py
@@ -137,7 +137,12 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/3.2/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LOCALE_PATHS = [os.path.join(BASE_DIR, 'locale')]
+LANGUAGES = [
+    ('en', 'English'),
+]
+
+#LANGUAGE_CODE = 'en-us'
 
 TIME_ZONE = 'UTC'
 
