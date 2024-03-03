ae7ce2c696847ee6715a24bb085a14acee0b7ca5
charles <gitauwairimu@gmail.com>
2024-03-03 11:20:22 +0300

    added host ip

--- website/settings.py
+++ website/settings.py
@@ -32,7 +32,7 @@ SECRET_KEY = '_iHJCTKQnNEvdQvVcIvcDC-S7f2qiR_wSMBBBkcMC0A'
 #DEBUG = config('DEBUG', cast=bool)
 #DEBUG = os.environ.get('DEBUG')
 DEBUG = True
-ALLOWED_HOSTS = ['52.91.47.197', 'cloud101.dev', 'localhost']
+ALLOWED_HOSTS = ['18.234.218.34', 'cloud101.dev', 'localhost']
 
 sys.modules['fontawesome_free'] = __import__('fontawesome-free')
 
