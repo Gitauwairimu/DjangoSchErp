0fd3afb160089bc9b4c3343dcb61945e109c15a1
charles <gitauwairimu@gmail.com>
2024-03-07 03:45:56 +0300

    import dotenv

--- website/settings.py
+++ website/settings.py
@@ -12,8 +12,11 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 
 import sys
 import os
+import dotenv
 from pathlib import Path
 from decouple import config
+
+dotenv.load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
